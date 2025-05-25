1ba33748442bb2d2983be9009f401d92699f96f3
monTzoncaloy <138038974+monTzoncaloy@users.noreply.github.com>
2025-05-25 16:26:16 +0800

    Caloy (#40)
    * Updated sidebar.jsx
    
    * added shadow to clockstyle.js
    
    * added shadow to directorystyle.js
    
    * Updated registerstyle.js
    
    * Update newmedschedstyle.js
    
    * Update updatemedstyle.js
    
    * Update updatepatientstyle.js
    
    * Update viewpatientstyle.js

--- Frontend/app/stylesheets/updatepatientstyle.js
+++ Frontend/app/stylesheets/updatepatientstyle.js
@@ -23,11 +23,16 @@ const styles = StyleSheet.create({
   },
   formContainer: {
     flexDirection: 'row',
-    backgroundColor: '#999999',
+    backgroundColor: '#b8cbdb',
     padding: 40,
     borderRadius: 10,
     justifyContent: 'space-between',
     maxHeight: 580,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    elevation: 8,
   },
   column: {
     flex: 1,
